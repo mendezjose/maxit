0185303ba270a631251dd3d0d1b577d86fd5cd3c
mendezjose <42753755+mendezjose@users.noreply.github.com>
2020-07-12 00:16:50 -0500

    first commit

---
+++ routes/jobs.js
@@ -0,0 +1,86 @@
+const validateObjectId = require("../middleware/validateObjectId");
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const { Job, validate } = require("../models/job");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+const regexGen = require("../utils/regexGen");
+
+router.get("/", async (req, res) => {
+  const q = req.query.q || "";
+  const l = req.query.l || "";
+
+  const jobs = await Job.find({ title: new RegExp(q, "i") })
+    .and({
+      $or: [
+        { "location.city": regexGen(l) },
+        { "location.state": regexGen(l) },
+      ],
+    })
+    .select("-__v")
+    .sort("title");
+  res.send(jobs);
+});
+
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let job = new Job({
+    title: req.body.title,
+    description: req.body.description,
+    jobType: req.body.jobType,
+    company: req.body.company,
+    location: {
+      country: req.body.location.country,
+      state: req.body.location.state,
+      city: req.body.location.city,
+    },
+    salary: {
+      mini: req.body.salary.mini,
+      maxi: req.body.salary.maxi,
+    },
+  });
+  job = await job.save();
+
+  res.send(job);
+});
+
+// router.put("/:id", [auth, validateObjectId], async (req, res) => {
+//   const { error } = validate(req.body);
+//   if (error) return res.status(400).send(error.details[0].message);
+
+//   const genre = await Genre.findByIdAndUpdate(
+//     req.params.id,
+//     { name: req.body.name },
+//     {
+//       new: true
+//     }
+//   );
+
+//   if (!genre)
+//     return res.status(404).send("The genre with the given ID was not found.");
+
+//   res.send(genre);
+// });
+
+// router.delete("/:id", [auth, admin, validateObjectId], async (req, res) => {
+//   const genre = await Genre.findByIdAndRemove(req.params.id);
+
+//   if (!genre)
+//     return res.status(404).send("The genre with the given ID was not found.");
+
+//   res.send(genre);
+// });
+
+router.get("/:id", async (req, res) => {
+  const job = await Job.findById(req.params.id).select("-__v");
+
+  if (!job)
+    return res.status(404).send("The genre with the given ID was not found.");
+
+  res.send(job);
+});
+
+module.exports = router;
